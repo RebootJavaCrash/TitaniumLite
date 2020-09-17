66953aa4d66f31735903ca20bb4f9df3b5cb7efb
RebootJavaCrash <57504733+RebootJavaCrash@users.noreply.github.com>
2020-09-17 09:28:04 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -5,7 +5,7 @@ var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
+
 </script>
 <html>
 <body>
