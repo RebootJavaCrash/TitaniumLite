75e08c849cfa03ea164f366f97cfd4614886f1c3
RebootJavaCrash <57504733+RebootJavaCrash@users.noreply.github.com>
2020-09-17 09:25:10 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,8 +1,22 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
+Welcome, <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
+
+<script>
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+</script>
+<p the current date is: </p>
+<script>
+  document.write(today);
+</script>
