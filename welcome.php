004ac8a5cabd0fc00e5e068b324b40eb6c5e42ce
RebootJavaCrash <57504733+RebootJavaCrash@users.noreply.github.com>
2020-09-23 21:30:24 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -11,7 +11,7 @@ document.write(today);
 <body>
 
 Welcome, <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+The password you imput is: <?php echo $_POST["password"]; ?>
 </body>
 </html>
 
